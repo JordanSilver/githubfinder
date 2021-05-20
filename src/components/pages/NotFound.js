7e39caff5092f1b2aeb5a01430597ee3dd950776
Daniel Silver <dabzbm@gmail.com>
2021-05-20 12:24:40 -0400

    first commit

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const NotFound = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p className='lead'>These are not the drones your looking for</p>
+    </div>
+  );
+};
+
+export default NotFound;
